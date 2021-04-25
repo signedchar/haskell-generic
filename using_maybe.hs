8f6a443f2fedcd2de58364890311c855dc1b053c
Erbeth Charte <echarte@tutanota.com>
2021-04-25 15:30:46 -0400

    Using Maybe Int Add some value to a Maybe Int addX (Just 3) 1 => Just 4 addX (Just (-1)) 0 => Just (-1) addX (Just 1) 0 => Just 1

--- using_maybe.hs
+++ using_maybe.hs
@@ -8,3 +8,7 @@ addOne :: Maybe Int -> Maybe Int
 addOne x = case x of
     (Just 0) -> Nothing
     (Just x) -> fmap (+1) (Just x)
+
+-- Add X to a `Maybe Int`
+addX :: Maybe Int -> Int -> Maybe Int
+addX (Just x) y = fmap (+y) (Just x)
