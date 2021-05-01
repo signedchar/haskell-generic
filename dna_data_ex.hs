e14725e72b8e169aad8e20948b607261aae65c1a
Erbeth Charte <echarte@tutanota.com>
2021-05-01 19:34:16 -0400

    Change match function.

--- dna_data_ex.hs
+++ dna_data_ex.hs
@@ -1,4 +1,4 @@
-Data import.List
+import Data.List
 
 data NtDNA = Ad | Td | Gd | Cd deriving (Eq)
 data NtRNA = Ar | Ur | Gr | Cr deriving (Eq)
@@ -12,8 +12,12 @@ class NucleotidChain a where
 instance NucleotidChain DNA where
   valid (DNA str) = all (\nuc -> (nuc == Ad) || (nuc == Td) || (nuc == Gd) || (nuc == Cd)) str
   countATGX (DNA str) =
-    let adenine = length $ Ad `elemIndices` str
-        thymine = length $ Td `elemIndices` str
-        guanine = length $ Gd `elemIndices` str
-        cytosine = length $ Cd `elemIndices` str
-    in (adenine, thymine, guanine, cytosine)
+    let a = length $ (filter (\nuc -> nuc == Ad) str)
+        t = length $ (filter (\nuc -> nuc == Td) str)
+        g = length $ (filter (\nuc -> nuc == Gd) str)
+        c = length $ (filter (\nuc -> nuc == Cd) str)
+    in (a, t, c, g)
+
+-- :l dna_data_ex
+-- countATGX (DNA [Td, Ad, Td, Ad, Gd, Cd, Cd, Cd, Ad])
+-- => (3,2,3,1)
