cc3522b3afbf611c82314741f4d661ee6e3d3462
Erbeth Charte <echarte@tutanota.com>
2021-05-01 19:34:34 -0400

    Functor instance for Maybe x.

--- using_maybe.hs
+++ using_maybe.hs
@@ -12,3 +12,15 @@ addOne x = case x of
 -- Add X to a `Maybe Int`
 addX :: Maybe Int -> Int -> Maybe Int
 addX (Just x) y = fmap (+y) (Just x)
+
+-- Functor instance with `Maybe a`
+class Functor f where
+  fmap :: (a -> b) -> fa -> fb --method
+
+data Maybe a = Nothing | Just a
+
+fmap :: (a -> b) -> Maybe a -> Maybe b
+
+instance Functor Maybe where
+  fmap f (Just x) = Just (f x)
+  fmap f Nothing = Nothing
