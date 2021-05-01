f8c73851abd0ae3ac7bf3e9a19cab2d543e07e6b
Erbeth Charte <echarte@tutanota.com>
2021-05-01 09:14:28 -0400

    data type Some example for DNA and RNA abstraction.

--- dna_data_ex.hs
+++ dna_data_ex.hs
@@ -1,8 +1,19 @@
+import Data.List
+
 data NtDNA = Ad | Td | Gd | Cd deriving (Eq)
 data NtRNA = Ar | Ur | Gr | Cr deriving (Eq)
-data DNA = [NtDNA] deriving (Eq)
-data RNA = [NtRNA] deriving (Eq)
+data DNA = DNA [NtDNA] deriving (Eq)
+data RNA = RNA [NtRNA] deriving (Eq)
 
 class NucleotidChain a where
   valid :: a -> Bool
-  countATGX :: (Int, Int, Int, Int)
+  countATGX :: a -> (Int, Int, Int, Int)
+
+instance NucleotidChain DNA where
+  valid (DNA str) = all (\nuc -> (nuc == Ad) || (nuc == Td) || (nuc == Gd) || (nuc == Cd)) str
+  countATGX (DNA str) =
+    let adenine = length $ Ad `elemIndices` str
+        thymine = length $ Td `elemIndices` str
+        guanine = length $ Gd `elemIndices` str
+        cytosine = length $ Cd `elemIndices` str
+    in (adenine, thymine, guanine, cytosine)
